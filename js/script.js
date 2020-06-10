b4108cf979193eaf3248677b30a619f8ba71d6d3
chages16 <chages16@gmail.com>
2020-06-10 23:29:35 +0930

    Created function to arrange by high score, updated media queries

--- js/script.js
+++ js/script.js
@@ -6,30 +6,31 @@ var inserthere = document.querySelector("#buttonshere");
 var initialsEl = document.getElementById("yourinitials");
 var answeredEl = document.getElementById("youranswered");
 var resultsEl = document.getElementById("yourresults");
-
-
+var resultsEl = document.getElementById("yourresults");
 var buttoncreate = document.createElement("button");
-console.log(question.textContent)
+var clearEl = document.getElementById("clearhighscores");
 
 var answer = ["a","b","c","d"];
-var correctanswer = ["a","d","b","a"]
-var questiontext = ["Question 1 Answer is A!",
+var correctanswer = ["a","d","b","a","d","c"]
+var questiontext = ['Question 1 Answer is <br> A!',
 "Question 2 Answer is D!",
 "Question 3 Answer is B!",
-"Question 4 Answer is A!"
+"Question 4 Answer is A!",
+"Question 5 Answer is D!",
+"Question 6 Answer is C!"
 ];
-
 var round = 0
 var ioutput = [];
 var qoutput = [];
 var soutput = [];
+var secondsLeft =60;
+var maxseconds = secondsLeft;
 
 beginningbutton.addEventListener("click", function(event) {
     event.preventDefault();
     console.log(event);
     document.getElementById("beginning").style="display: none;";
     alert("hello");
-    secondsLeft=80;
     question.textContent= questiontext[round];
     setTime();
     
@@ -55,20 +56,14 @@ beginningbutton.addEventListener("click", function(event) {
       if(secondsLeft < 0.1) {
         clearInterval(timerInterval);
         alert("Too late");
-        var totalscore = 0
-        var totalscore= secondsLeft+round;  
-        alert("Your Score (Questions answered + time left) is " + totalscore);
-        clearInterval(timerInterval);  
-        insertinitials()    
-        location=location
-      }
+        secondsLeft = 0
+        insertinitials()
+        clearInterval(timerInterval);
+        }
   
       if(round == correctanswer.length) {  
-        var totalscore = 0
-        var totalscore= secondsLeft+round;  
-        alert("Your Score (Questions answered + time left) is " + totalscore);
-        clearInterval(timerInterval);  
-        insertinitials()    
+        insertinitials()
+        clearInterval(timerInterval);
       }
 
     }, 1000);
@@ -76,11 +71,19 @@ beginningbutton.addEventListener("click", function(event) {
     
 
     function insertinitials() {
-      var totalscore= secondsLeft+round;
+      var totalscore = 0;
+      var totalscore= secondsLeft+round;  
+      alert("Your score (questions answered plus your time left) is " + totalscore);
       var initials = prompt("Please type your initials here");
       if (initials === null || initials =="") {
-        alert("Please enter initials");
-        insertinitials();
+        var confirminit = confirm("Please enter initials");
+        if (confirminit == true){
+          insertinitials();
+        }
+        else {
+          location=location
+          return
+        }
     }
       if (initials.length> 3){
         alert("too long- less then 3 initials please");
@@ -94,6 +97,9 @@ beginningbutton.addEventListener("click", function(event) {
         localStorage.setItem("answered", JSON.stringify(qoutput));
         localStorage.setItem("totalscore", JSON.stringify(soutput));
         restorescores();
+        location=location   
+
+        
 
     }
       
@@ -134,22 +140,54 @@ beginningbutton.addEventListener("click", function(event) {
       console.log(storedinitials)
       console.log(storedtotal)
     }  
-      for (var i = 0; i < ioutput.length; i++) {
-        var li1 = document.createElement("h4");
-        console.log("ioutput= " + ioutput[i]);
-        li1.textContent = ioutput[i];
+    tabulatescores();
+  }
+
+  function tabulatescores() {
+      var indices = [];
+      for (var tt= correctanswer.length+maxseconds;tt>-0.5;tt--){
+      var element = tt;
+      var idx = soutput.indexOf(element);
+      while (idx != -1) {
+      indices.push(idx);
+      idx = soutput.indexOf(element, idx + 1);
+      }
+    }
+      for (var i = 0; i < indices.length; i++) {
+        i2 = indices[i]
+        console.log(i2)
+        var li1 = document.createElement("h5");
+        console.log("ioutput= " + ioutput[i2]);
+        li1.textContent = ioutput[i2];
         console.log("Li1 = " + li1.textContent);
         initialsEl.appendChild(li1);
-        var li2 = document.createElement("h4");
-        console.log("qoutput= " + qoutput[i]);
-        li2.textContent = qoutput[i];
+        var li2 = document.createElement("h5");
+        console.log("qoutput= " + qoutput[i2]);
+        li2.textContent = qoutput[i2];
         console.log("Li2 = " + li2.textContent);
         answeredEl.appendChild(li2);
-        var li3 = document.createElement("h4");
-        console.log("soutput= " + soutput[i]);
-        li3.textContent = soutput[i];
+        var li3 = document.createElement("h5");
+        console.log("soutput= " + soutput[i2]);
+        li3.textContent = soutput[i2];
         console.log("Li3 = " + li3.textContent);
         resultsEl.appendChild(li3);
     }
+    
+    console.log(indices);
   }
+
   restorescores();
+  
+  clearEl.addEventListener("click",function resetscores() {
+    localStorage.setItem("initials","");
+    localStorage.setItem("answered","");
+    localStorage.setItem("totalscore","");
+    resultsEl.innerHTML = "";
+    answeredEl.innerHTML = "";
+    initialsEl.innerHTML = "";
+    ioutput = [];
+    qoutput = [];
+    soutput = [];
+  })
+
+  
