021f9e580bd6fc56d897bee35dafeef3c1c79728
chages16 <chages16@gmail.com>
2020-06-11 19:03:15 +0930

    Adding questions

--- js/script.js
+++ js/script.js
@@ -6,11 +6,13 @@ var inserthere = document.querySelector("#buttonshere");
 var initialsEl = document.getElementById("yourinitials");
 var answeredEl = document.getElementById("youranswered");
 var resultsEl = document.getElementById("yourresults");
-var resultsEl = document.getElementById("yourresults");
+
+
 var buttoncreate = document.createElement("button");
-var clearEl = document.getElementById("clearhighscores");
+console.log(question.textContent)
 
 var answer = ["a","b","c","d"];
+<<<<<<< HEAD
 var correctanswer = ["a","d","b","a","d","c"]
 var questiontext = ['What best decribes Javascript? <br><br> A) A programming language that supports event-driven, functional, and imperative programming styles. <br> B) Used to define styles for your web pages, including the design, layout and variations in display for different devices and screen sizes.<br>C) The standard markup language for Web pages<br>D)a structured set of data held in a computer, especially one that is accessible in various ways.',
 "What following command will send information to the broswer console? <br><br> A) event.console <br> B) log.event <br>C) event.log <br>D)console.log",
@@ -18,20 +20,31 @@ var questiontext = ['What best decribes Javascript? <br><br> A) A programming la
 'Which of these is the best for finding the element with the id type-"beginning"? <br><br> A) document.querySelector("#beginning");<br> B) document.querySelector(".beginning"); <br>C) document.getElementById("#beginning");<br>D) document.getElementById("id = beginning")',
 'What would "array[4]" return for "var array = [1,2,3,4,5,6]"? <br><br> A) -1 <br>B) 4 <br>C) undefined;<br>D) 5',
 'If var a ="2" and var b="1" what would b + a equal? <br><br> A) 3 <br>B) "21" <br>C) "12"<br>D) undefined'
+=======
+var correctanswer = ["a","d","b","a"]
+var questiontext = ["Question 1 Answer is A!",
+"Question 2 Answer is D!",
+"Question 3 Answer is B!",
+"Question 4 Answer is A!"
+>>>>>>> parent of b4108cf... Created function to arrange by high score, updated media queries
 ];
+
 var round = 0
 var ioutput = [];
 var qoutput = [];
 var soutput = [];
-var secondsLeft =60;
-var maxseconds = secondsLeft;
 
 beginningbutton.addEventListener("click", function(event) {
     event.preventDefault();
     console.log(event);
     document.getElementById("beginning").style="display: none;";
     alert("hello");
+<<<<<<< HEAD
     question.innerHTML= questiontext[round];
+=======
+    secondsLeft=80;
+    question.textContent= questiontext[round];
+>>>>>>> parent of b4108cf... Created function to arrange by high score, updated media queries
     setTime();
     
     for(i=0;i<4;i++) {
@@ -56,14 +69,20 @@ beginningbutton.addEventListener("click", function(event) {
       if(secondsLeft < 0.1) {
         clearInterval(timerInterval);
         alert("Too late");
-        secondsLeft = 0
-        insertinitials()
-        clearInterval(timerInterval);
-        }
+        var totalscore = 0
+        var totalscore= secondsLeft+round;  
+        alert("Your Score (Questions answered + time left) is " + totalscore);
+        clearInterval(timerInterval);  
+        insertinitials()    
+        location=location
+      }
   
       if(round == correctanswer.length) {  
-        insertinitials()
-        clearInterval(timerInterval);
+        var totalscore = 0
+        var totalscore= secondsLeft+round;  
+        alert("Your Score (Questions answered + time left) is " + totalscore);
+        clearInterval(timerInterval);  
+        insertinitials()    
       }
 
     }, 1000);
@@ -71,19 +90,11 @@ beginningbutton.addEventListener("click", function(event) {
     
 
     function insertinitials() {
-      var totalscore = 0;
-      var totalscore= secondsLeft+round;  
-      alert("Your score (questions answered plus your time left) is " + totalscore);
+      var totalscore= secondsLeft+round;
       var initials = prompt("Please type your initials here");
       if (initials === null || initials =="") {
-        var confirminit = confirm("Please enter initials");
-        if (confirminit == true){
-          insertinitials();
-        }
-        else {
-          location=location
-          return
-        }
+        alert("Please enter initials");
+        insertinitials();
     }
       if (initials.length> 3){
         alert("too long- less then 3 initials please");
@@ -97,9 +108,6 @@ beginningbutton.addEventListener("click", function(event) {
         localStorage.setItem("answered", JSON.stringify(qoutput));
         localStorage.setItem("totalscore", JSON.stringify(soutput));
         restorescores();
-        location=location   
-
-        
 
     }
       
@@ -140,54 +148,22 @@ beginningbutton.addEventListener("click", function(event) {
       console.log(storedinitials)
       console.log(storedtotal)
     }  
-    tabulatescores();
-  }
-
-  function tabulatescores() {
-      var indices = [];
-      for (var tt= correctanswer.length+maxseconds;tt>-0.5;tt--){
-      var element = tt;
-      var idx = soutput.indexOf(element);
-      while (idx != -1) {
-      indices.push(idx);
-      idx = soutput.indexOf(element, idx + 1);
-      }
-    }
-      for (var i = 0; i < indices.length; i++) {
-        i2 = indices[i]
-        console.log(i2)
-        var li1 = document.createElement("h5");
-        console.log("ioutput= " + ioutput[i2]);
-        li1.textContent = ioutput[i2];
+      for (var i = 0; i < ioutput.length; i++) {
+        var li1 = document.createElement("h4");
+        console.log("ioutput= " + ioutput[i]);
+        li1.textContent = ioutput[i];
         console.log("Li1 = " + li1.textContent);
         initialsEl.appendChild(li1);
-        var li2 = document.createElement("h5");
-        console.log("qoutput= " + qoutput[i2]);
-        li2.textContent = qoutput[i2];
+        var li2 = document.createElement("h4");
+        console.log("qoutput= " + qoutput[i]);
+        li2.textContent = qoutput[i];
         console.log("Li2 = " + li2.textContent);
         answeredEl.appendChild(li2);
-        var li3 = document.createElement("h5");
-        console.log("soutput= " + soutput[i2]);
-        li3.textContent = soutput[i2];
+        var li3 = document.createElement("h4");
+        console.log("soutput= " + soutput[i]);
+        li3.textContent = soutput[i];
         console.log("Li3 = " + li3.textContent);
         resultsEl.appendChild(li3);
     }
-    
-    console.log(indices);
   }
-
   restorescores();
-  
-  clearEl.addEventListener("click",function resetscores() {
-    localStorage.setItem("initials","");
-    localStorage.setItem("answered","");
-    localStorage.setItem("totalscore","");
-    resultsEl.innerHTML = "";
-    answeredEl.innerHTML = "";
-    initialsEl.innerHTML = "";
-    ioutput = [];
-    qoutput = [];
-    soutput = [];
-  })
-
-  
